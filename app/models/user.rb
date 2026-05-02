a144076f0995cc30a28d64836d52b0686938dac2
MuhammadNaushad <mnaushad173@gmail.com>
2026-05-02 18:33:57 +0530

    add email.downcase to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+before_save { self.email = email.downcase }
+
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   EMAIL_REGEX =  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
