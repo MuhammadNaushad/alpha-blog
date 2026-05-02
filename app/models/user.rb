78935bc0df53fc5209d8a5c4a828622fc18062b1
MuhammadNaushad <mnaushad173@gmail.com>
2026-05-02 16:06:09 +0530

    add user validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  EMAIL_REGEX =  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  # validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
 end
