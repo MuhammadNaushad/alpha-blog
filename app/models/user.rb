62bc1ef23436dd4b08653ae47695a73a341cd225
MuhammadNaushad <mnaushad173@gmail.com>
2026-05-03 07:08:00 +0530

    add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ before_save { self.email = email.downcase }
   EMAIL_REGEX =  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   # validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
+
+  has_secure_password
 end
