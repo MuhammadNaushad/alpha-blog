5947f59452abeedb8656c8415e959d4479360fed
MuhammadNaushad <mnaushad173@gmail.com>
2026-05-03 13:05:32 +0530

    add user show page feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    render "auth/show"
+  end
+
   def new
     @user = User.new
     render "auth/new"
