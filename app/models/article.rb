3a59381f0852bc10a8ecee9e4222b585ecfcc2e9
MuhammadNaushad <mnaushad173@gmail.com>
2026-05-15 12:47:23 +0530

    completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user # class_name: "users", foreign_key: "users_id"
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 end
